4f1e06ba0b17b122654362ede8abdcca4fd83d29
enboampong <boampongjoe44@gmail.com>
2023-08-04 04:01:42 +0000

    works well

--- trees-cl-approach.py
+++ trees-cl-approach.py
@@ -1,30 +1,45 @@
-
-
-
-
 def trees_crediting_level_approach():
-    iteration = 0
-    iteration2 = 0
-    crediting_level = 0
-    emissions = []
-    total_emissions = 0
-    number_of_years = len(emissions)
-    while number_of_years <= 5:
-        emission = input("Kindly enter the emission value for year "+ str(iteration +1) + ": ")
-        if emission == '':
-            emission = 0
-        emissions.append(int(emission))
-        iteration +=1
-    for emission in emissions:
-        total_emissions +=emission
-    crediting_level = total_emissions/number_of_years
-    print("The Trees Crediting Level for the period of ", number_of_years , " years is" , crediting_level)
+    period_emissions=[]
+    annual_emissions=[]
+    
+    def get_reference_period_emissions():
+        iteration=0
+        print("----------------------------------------------------------------------")
+        while iteration < 5:
+            period_emission = input("Kindly enter the emission value for year "+ str(iteration+1)+ " : ")
+            period_emissions.append(float(period_emission))
+            iteration += 1
 
-    while iteration2 < number_of_years:
-        print("The Annual Emmission Reduction for year", (iteration2 + 1) ,"is " , (crediting_level - emissions[iteration2]))
-        iteration2 +=1
-    return 0
+    def get_annual_emissions():
+        iteration = 0
+        print("----------------------------------------------------------------------")
+        while iteration < 5:
+            annual_emission = input("kindly enter the annual emission for year "+ str(iteration+1)+ " : ")
+            annual_emissions.append(annual_emission)
+            iteration +=1
+    
+    def get_crediting_level():
+        sum_emissions = 0
+        for emission in period_emissions:
+            sum_emissions = float(emission) + sum_emissions
+        crediting_level = (sum_emissions / 5)
+        return crediting_level
+    
+    def get_annual_emission_reductions():
+        iteration = 0
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print("The Crediting level is : "+ str(get_crediting_level()))
+        while iteration < 5:
+            annual_emission_reduction = get_crediting_level() - float(annual_emissions[iteration])
+            print("_________________________________________________________________________")
+            print("The Annual Emmission Reduction for year "+ str(iteration + 1) +" is :    " + str(get_crediting_level() - float(annual_emissions[iteration])))
+            iteration += 1
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
+    get_reference_period_emissions()
+    get_annual_emissions()
+    get_annual_emission_reductions()       
+    
 trees_crediting_level_approach()
 
 
--- trees-hfld-approach.py
+++ trees-hfld-approach.py
@@ -5,30 +5,44 @@ def trees_hfld_approach():
     annual_emissions = []
     annual_foregone_removals = []
     hfld_scores = [ ]
+    percentage_inceases = []
+    deduction_values = []
+    emission_reductions = []
     iteration = 0
+    penalties = []
+    total_emission_reductions = []
     
     carbon_stock = input("Kindly enter the Total Carbon Stock in above and below ground tree biomass : ")
 
     def get_reference_period_deforestation_rates():
         iteration = 0
+        print("----------------------------------------------------------------------")
         while len(reference_period_deforestation_rates) < 5:            
-            reference_period_deforestation_rate = input("Kindly enter the reference period deforestation rate percentage value for year "+ str(iteration + 1) +" : ")
-            if reference_period_deforestation_rate == '':
+            reference_period_deforestation_rate_input = input("Kindly enter the reference period deforestation rate percentage value for year "+ str(iteration + 1) +" : ")
+            if reference_period_deforestation_rate_input:
+                reference_period_deforestation_rate = (float(reference_period_deforestation_rate_input)/100.00)
+            else:
                 reference_period_deforestation_rate = 0
             reference_period_deforestation_rates.append(reference_period_deforestation_rate)
             iteration += 1
-
+        
+        
     def get_reference_period_forest_covers(): 
         iteration = 0  
+        print("----------------------------------------------------------------------")
         while len(reference_period_forest_covers) < 5:            
-            reference_period_forest_cover = input("Kindly enter the reference period forest cover value for year "+ str(iteration + 1)+" : ")
-            if reference_period_forest_cover == '':
+            reference_period_forest_cover_input = input("Kindly enter the reference period forest cover value for year "+ str(iteration + 1)+" : ")
+            if reference_period_forest_cover_input :
+                reference_period_forest_cover = (float(reference_period_forest_cover_input)/100.00)
+            else:
                 reference_period_forest_cover = 0
             reference_period_forest_covers.append(reference_period_forest_cover)
             iteration += 1
-
+        
+        
     def get_reference_period_emissions(): 
         iteration = 0   
+        print("----------------------------------------------------------------------")
         while len(reference_period_emissions) < 5:
             reference_period_emission = input("Kindly enter the reference period emission value for year "+ str(iteration + 1)+" : ")
             if reference_period_emission == '':
@@ -38,15 +52,18 @@ def trees_hfld_approach():
 
     def get_annual_emissions():
         iteration = 0
+        print("----------------------------------------------------------------------")
         while len(annual_emissions) < 5:
             annual_emission = input("Kindly enter the annual emission value for year "+ str(iteration + 1)+" : ")
             if annual_emission == '':
                 annual_emission = 0
             annual_emissions.append(annual_emission)
             iteration += 1
+        
 
     def get_annual_foregone_removals():
         iteration = 0
+        print("----------------------------------------------------------------------")
         while len(annual_foregone_removals) < 5:
             annual_foregone_removal = input("Kindly enter the annual foregone removal value for year "+ str(iteration + 1)+" : ")
             if annual_foregone_removal == '':
@@ -60,29 +77,116 @@ def trees_hfld_approach():
     get_annual_emissions()
     get_annual_foregone_removals()
 #========================================================================================================================================================
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     while iteration < 5:
-        hfld_score= (((int(reference_period_forest_covers[iteration])*100-50)/100) + (0.5- int(reference_period_deforestation_rates[iteration])*100))
+        hfld_score= (((float(reference_period_forest_covers[iteration])*100.00-50.00)/100.00) + (0.5- float(reference_period_deforestation_rates[iteration])*100.00))
         hfld_scores.append(hfld_score)
-        if hfld_score < 0.5:
+        if hfld_score > 0.5:
             threshold = " exceeds the threshold"
         else:
             threshold = " does not exceed the threshold"
-        print("The HFLD score for reference year "+ str(iteration + 1) + " is" + str(hfld_score) + ". It "+  threshold)
+        print("The HFLD score for reference year "+ str(iteration + 1) + " is  " + str(hfld_score) + " . It "+  threshold)
         iteration += 1
     sum_scores=0
     for score in hfld_scores:
         sum_scores = sum_scores + score
     avg_hfld_score = sum_scores/len(hfld_scores)
+    
     print ("The Average HFLD Score is :"+ str(avg_hfld_score))
 
-#=========================================================================================================================================================
-    sum_emissions = 0
-    for emission in reference_period_emissions:
-        sum_emissions = int(emission) + sum_emissions
-    crediting_level = sum_emissions / 5
-    hfld_crediting_level = (crediting_level + (avg_hfld_score * (0.0005 * int(carbon_stock))))
-    print("The HFLD Crediting level is "+ str(hfld_crediting_level))
+#========================================================================================================================================================
+    def get_crediting_level():
+        sum_emissions = 0
+        for emission in reference_period_emissions:
+            sum_emissions = float(emission) + sum_emissions
+        crediting_level = sum_emissions / 5
+        return crediting_level
 
 #=========================================================================================================================================================
+    def get_hfld_crediting_level():  
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")  
+        crediting_level =  get_crediting_level()
+        hfld_crediting_level = (crediting_level + (avg_hfld_score * (0.0005 * float(carbon_stock))))
+        print("The HFLD Crediting level is "+ str(hfld_crediting_level))
+        return hfld_crediting_level
+    #get_hfld_crediting_level()
+    hfld_crediting_lev = get_hfld_crediting_level()
+#=========================================================================================================================================================
+    def get_percentage_increase():
+        iteration = 0
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        crediting_level = get_crediting_level()
+        while iteration < 5:
+            if hfld_crediting_lev:
+                percentage_increase = ((float(annual_emissions[iteration]) - float(crediting_level))/ float(crediting_level))
+                print("The Percentage increase for Year " + str(iteration+1) + " is : " + str(percentage_increase*100)+"%")
+                percentage_inceases.append(percentage_increase)      
+            else :
+                percentage_increase = 0  
+            iteration += 1
+    
 
+#==========================================================================================================================================================
+    def get_deduction_value():
+        iteration = 0
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        while iteration < 5:
+            
+            if float(percentage_inceases[iteration]) < 0.15 :
+                deduction_value = 0
+            else:
+                if float(percentage_inceases[iteration]) < 0.35 :
+                    deduction_value = 0.15
+                else: 
+                    if float(percentage_inceases[iteration]) < 0.55 :
+                        deduction_value = 0.25
+                    else:
+                        if float(percentage_inceases[iteration]) < 0.75 :
+                            deduction_value = 0.35
+                        else:
+                            deduction_value = 1
+            print("The Deduction Value for Year " + str(iteration+1) + " is : " + str(deduction_value*100)+ "%")
+            deduction_values.append(deduction_value)
+            iteration += 1
+
+    
+#  ==========================================================================================================================================================
+
+    def get_emission_reduction():
+        iteration = 0
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        while iteration < 5:
+            emission_reduction = (hfld_crediting_lev - float(annual_emissions[iteration]))
+            emission_reductions.append(emission_reduction)
+            print("The emission reduction for Year " + str(iteration+1) + " is : " + str(emission_reduction))
+            iteration += 1
+        
+#============================================================================================================================================================            
+    
+    def get_penalty():
+        iteration = 0
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        while iteration < 5:
+            penalty = ((float(emission_reductions[iteration]) + float(annual_foregone_removals[iteration])) * float(deduction_values[iteration]))
+            penalties.append(penalty)
+            print("The HFLD Penalty for Year " + str(iteration+1) + " is : " + str(penalty))
+            iteration += 1
+            
+#============================================================================================================================================================            
+            
+    def get_hfld_total_emission_reduction():
+        iteration = 0
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        while iteration < 5:
+            total_emission_reduction = ((float(emission_reductions[iteration]) + float(annual_foregone_removals[iteration])) - float(penalties[iteration]))
+            total_emission_reductions.append(total_emission_reduction)
+            print("The Total HFLD Emissions Reductions for Year " + str(iteration+1) + " is : " + str(total_emission_reduction))
+            iteration += 1
+     
+    get_percentage_increase()
+    get_deduction_value()
+    get_emission_reduction()  
+    get_penalty()
+    get_hfld_total_emission_reduction() 
+    
 trees_hfld_approach()
